10c162367ca593b9e49d847fca26f1b348964894
Paulo Mateus <paulomatew@gmail.com>
2019-05-10 15:26:08 -0300

    Pegando erros e parando execução.

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
+set LOG=log.log
+call > %LOG%
+REM LIMPANDO ARQUIVO DE LOG
+
+
 REM OBRIGATORIO DEFINIR CAMINHO DO GRADLE EM VARIAVEL DO SISTEMA (PATH). EX: "C:\Java\gradle-2.14\bin\".
 REM OBRIGATORIO DEFINIR CAMINHO DO JAVA EM VARIAVEL DO SISTEMA (PATH). EX: "C:\Java\jdk1.8.0_201\bin".
 REM ESSE ARQUIVO DEVE ESTAR NO MESMO NIVEL DAS PASTAS TASY, TASY-BACKEND, TASY-FRAMEOWRK-BACKEND E TASY-FRAMEWORK. EX:
@@ -10,38 +15,40 @@ REM --> build.bat
 REM AUTOMATIZAÇÃO PARA ATUALIZAR, BUILDAR E PUBLICAR PROJETOS LOCALMENTE, DE ACORDO COM UMA VERSÃO.
 REM REQUER QUE REPOSITORIOS TENHAM SIDO CLONADOS COM SSH.
 
-rem set frameworks_branch=3.02.1746
-rem set setor_branch=3.02.1746
-set frameworks_branch=dev
-set setor_branch=assistencial
+set frameworks_branch=3.02.1742
+set setor_branch=3.02.1742
+rem set frameworks_branch=dev
+rem set setor_branch=assistencial
+
 
 rem ===========================================================
 rem ===========================================================
 rem ===========================================================
+rem findstr /I "error"  %LOG%
 
 echo ===============================================
-echo Iniciando atualizacao tasy-framework-backend...
+echo Iniciando atualizacao tasy-framework-backend (pull)...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-framework-backend/ && git reset --hard && git checkout %frameworks_branch% && git pull"
+start /b /wait cmd.exe /c "cd tasy-framework-backend/ && git reset --hard && git checkout %frameworks_branch% && git pull" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo tasy-framework-backend atualizado com sucesso.
 echo ===============================================
-echo Rodando build tasy-framework-backend...
+echo Iniciando build tasy-framework-backend...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-framework-backend/ && gradle clean build"
+start /b /wait cmd.exe /c "cd tasy-framework-backend/ && gradle clean build" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo Build tasy-framework-backend finalizada com sucesso.
 echo ===============================================
 echo Publish do tasy-framework-backend vai iniciar.
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-framework-backend/ && gradle publish publishToMavenLocal"
+start /b /wait cmd.exe /c "cd tasy-framework-backend/ && gradle publish publishToMavenLocal" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo Publish do tasy-framework-backend finalizado com sucesso.
 echo ===============================================
 
@@ -49,21 +56,20 @@ rem ===========================================================
 rem ===========================================================
 rem ===========================================================
 
-echo ===============================================
-echo Iniciando atualizacao tasy-backend...
+echo Iniciando atualizacao tasy-backend (pull)...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-backend/ && git reset --hard && git checkout %setor_branch% && git pull"
+start /b /wait cmd.exe /c "cd tasy-backend/ && git reset --hard && git checkout %setor_branch% && git pull" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo tasy-backend atualizado com sucesso.
 echo ===============================================
-echo Rodando build tasy-backend...
+echo Iniciando build tasy-backend...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-backend/ && gradle clean build"
+start /b /wait cmd.exe /c "cd tasy-backend/ && gradle clean build" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo Build tasy-backend finalizada com sucesso.
 echo ===============================================
 
@@ -71,21 +77,20 @@ rem ===========================================================
 rem ===========================================================
 rem ===========================================================
 
-echo ===============================================
-echo Iniciando atualizacao tasy-framework...
+echo Iniciando atualizacao tasy-framework (pull)...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-framework/ && git reset --hard && git checkout %frameworks_branch% && git pull"
+start /b /wait cmd.exe /c "cd tasy-framework/ && git reset --hard && git checkout %frameworks_branch% && git pull" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo tasy-framework atualizado com sucesso.
 echo ===============================================
-echo Rodando install tasy-framework...
+echo Iniciando install tasy-framework...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy-framework/ && npm install"
+start /b /wait cmd.exe /c "cd tasy-framework/ && npm install" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
-echo ===============================================
 echo Install tasy-framework finalizado com sucesso.
 echo ===============================================
 
@@ -93,30 +98,40 @@ rem ===========================================================
 rem ===========================================================
 rem ===========================================================
 
-echo ===============================================
-echo Iniciando atualizacao tasy...
-echo ===============================================
-
-start /b /wait cmd.exe /c "cd tasy/ && git reset --hard && git checkout %setor_branch% && git pull"
-
-echo ===============================================
-echo tasy atualizado com sucesso.
-echo ===============================================
-echo Rodando install tasy...
+echo Iniciando atualizacao tasy frontend (pull)...
 echo ===============================================
 
-start /b /wait cmd.exe /c "cd tasy/ && npm install"
+start /b /wait cmd.exe /c "cd tasy/ && git reset --hard && git checkout %setor_branch% && git pull" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
 
+echo tasy frontend atualizado com sucesso.
 echo ===============================================
-echo Install tasy finalizado com sucesso.
-echo ===============================================
-echo Bower tasy vai iniciar.
-echo ===============================================
-
-start /b /wait cmd.exe /c "cd tasy/ && bower update"
-
-echo ===============================================
-echo Bower tasy finalizado com sucesso.
+echo Iniciando install tasy frontend...
 echo ===============================================
 
+start /b /wait cmd.exe /c "cd tasy/ && npm install" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
+
+echo Install tasy frontend finalizado com sucesso.
+echo ===============================================
+echo Bower tasy frontend vai iniciar.
+echo ===============================================
+
+start /b /wait cmd.exe /c "cd tasy/ && bower update" >> %LOG% 2>&1
+for /f "tokens=*" %%a in ('findstr /i "error failure fatal" %LOG%') do ( goto errorlabel )
+
+echo Bower tasy frontend finalizado com sucesso.
+echo ===============================================
+goto final
+
+:errorlabel
+echo ===============================================
+echo ===============================================
+echo FOI ENCONTRADO ERRO(s) NA(s) LINHA(s) ABAIXO:
+findstr /I "error failure fatal" %LOG%
+
+:final
+echo ===============================================
+echo ===============================================
+echo ===============================================
 set /p DUMMY=Processo finalizado. Pressione ENTER para finalizar...
